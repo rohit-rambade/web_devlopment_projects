40d9a09d36416c564596a65dd8109608dc6f7699
rohit-rambade <rohitrambade18@gmail.com>
2022-04-13 20:15:12 +0530

    JS

---
+++ Project#1_Modal/script.js
@@ -0,0 +1,13 @@
+const openBtn = document.querySelector('.open')
+const modalContainer = document.querySelector('.modal-container')
+
+const closeBtn = document.querySelector('.modal-btn')
+// console.log(openBtn)
+
+openBtn.addEventListener('click',()=>{
+    modalContainer.classList.add("show")
+})
+
+closeBtn.addEventListener('click',()=>{
+    modalContainer.classList.remove("show")
+})
